d30d5d72a5f0cfeb1636ae33a69857ea617fb35f
Sumit Upadhyay <skumarupadhyay1@gmail.com>
2025-12-24 00:36:49 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Sumit Upadhyay on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
